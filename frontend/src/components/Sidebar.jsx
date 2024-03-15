20669a2e431cee5e1ef2ad653f659ca40f075cea
Memoryman2022 <rlacannon@gmail.com>
2024-03-15 16:47:11 +0100

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// import "./Sidebar.css";
 
-export default function Sidebar({}) {
+export default function Sidebar() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsSidebarVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsSidebarVisible(false);
+  };
   return (
-    <div className="sidebar-container">
-      <h2>SideBar</h2>
+    <div
+      className="sidebar-container"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div className={`sidebar ${isSidebarVisible ? "visible" : "hidden"}`}>
+        <h2>SideBar</h2>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import focusImage from "../assets/focus.png";
 
 function Welcome() {
   return (
     <div className="welcome-container">
       <div className="welcome-div">
-        <h2>Welcome Page</h2>
+        <img className="logo-image" src={focusImage} alt="Focus" />
+        <h2>LOGIN</h2>
         <form>
           <label>
             <input type="text" name="name" placeholder="NAME" />
