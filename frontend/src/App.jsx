5b28e3b502028bea9ffa4562fb5d61ed7001a033
Memoryman2022 <rlacannon@gmail.com>
2024-03-15 14:46:03 +0100

    welcome

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/react.svg";
 //pages
+import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
@@ -22,6 +23,7 @@ function App() {
         <div className="main-container">
           <Sidebar />
           <Routes>
+            <Route path="/" element={<Welcome />} />
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="*" element={<NotFound />} />
---
+++ frontend/src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Welcome() {
+  return (
+    <div className="welcome-container">
+      <div className="welcome-Div">
+        <h2>Welcome Page</h2>
+      </div>
+    </div>
+  );
+}
+export default Welcome;
