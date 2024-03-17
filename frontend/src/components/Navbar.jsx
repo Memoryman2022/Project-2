871fa58677eccf3acdc033fe4c19dcdbaae0bde8
Memoryman2022 <rlacannon@gmail.com>
2024-03-17 14:12:44 +0100

    new logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 import React from "react";
-
+import logo from "../assets/FocusLogo_icon.png";
 export default function Navbar() {
   const navigate = useNavigate();
 
   return (
     <nav>
+      <img className="nav-logo" src={logo} alt="image-logo" />
       <ul className="navbar-list">
         <li className="navbar-item">
           <Link to="/welcome">Welcome</Link>
--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import focusImage from "../assets/focus.png";
+import focusImage from "../assets/FocusLogo.png";
+import { useState } from "react";
 
 function Welcome() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <div className="welcome-container">
-      <div className="welcome-div">
-        <img className="logo-image" src={focusImage} alt="Focus" />
-        <h2>LOGIN</h2>
-        <form>
-          <label>
-            <input type="text" name="name" placeholder="NAME" />
-          </label>
-          <label>
-            <input type="text" password="password" placeholder="PASSWORD" />
-          </label>
-          <div className="login-buttons-div">
-            <button className="login-button" type="submit">
-              SUBMIT
-            </button>
-          </div>
-        </form>
-      </div>
+      {showLogin ? (
+        <div className="welcome-div">
+          <img className="logo-image" src={focusImage} alt="Focus" />
+          <h2>LOGIN</h2>
+          <form>
+            <label>
+              <input type="text" name="name" placeholder="NAME" />
+            </label>
+            <label>
+              <input type="text" password="password" placeholder="PASSWORD" />
+            </label>
+            <div className="login-buttons-div">
+              <button className="login-button" type="submit">
+                SUBMIT
+              </button>
+            </div>
+          </form>
+        </div>
+      ) : (
+        <button onClick={() => setShowLogin(true)}>Show Login</button>
+      )}
     </div>
   );
 }
