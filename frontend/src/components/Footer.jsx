01031c9abb8eaeb37fed8b7f70718ef0f029e610
Memoryman2022 <rlacannon@gmail.com>
2024-03-18 17:44:15 +0100

    monday final

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,22 @@
 import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return <div className="footer">FOOTER</div>;
+  return (
+    <div className="footer">
+      <ul className="footer-list">
+        <li className="footer-item">
+          <Link to="/">FAQ</Link>
+        </li>
+        <li className="footer-item">
+          <Link to="/">CONTACT</Link>
+        </li>
+        <li className="footer-item">
+          <Link to="/">TERMS OF USE</Link>
+        </li>
+        <li className="footer-item">
+          <Link to="/">LINKS</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
