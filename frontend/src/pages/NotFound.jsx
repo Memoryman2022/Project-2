44328e1fa7c43858ddccfa966ca48242661fb483
Memoryman2022 <rlacannon@gmail.com>
2024-03-18 10:16:40 +0100

    not found image

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import cutImage from "../assets/video-editing.png";
 
 function NotFound() {
   return (
     <div className="notFound-container">
-      <h2>Not Found</h2>
+      <img className="cut-image" src={cutImage} alt="cut" />
+      <h2>CUT!!!</h2>
+      <br />
+      <h3>Not Found</h3>
+      <br />
+      <p>Lets try that again, shall we?</p>
     </div>
   );
 }
