e389016c51d0ef3d83a6e769c883b150429f4ec7
Memoryman2022 <rlacannon@gmail.com>
2024-03-20 22:42:08 +0100

    small styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,28 @@
 import { Link } from "react-router-dom";
-import movies from "../assets/movies.png";
-import series from "../assets/series.jpg";
+import movies from "../assets/video-camera.png";
+import series from "../assets/tv-monitor.png";
 
 function Home() {
-	return (
-		<div className="containerSelect animatedBakground">
-			<h2 className="titleSelect">Select Movies or Series</h2>
+  return (
+    <div className="containerSelect animatedBakground">
+      <h2 className="titleSelect">Select Movies or Series</h2>
 
-			<div className="imgSelect">
-				<Link to="/ListMovies">
-					<div className="labelMovies">
-						<img src={movies} alt="Movies" />
-						<label className="titleSelect"> MOVIES</label>
-					</div>
-				</Link>
-				<Link to="/ListSeries">
-					<div className="labelMovies">
-						<img src={series} alt="Series" />
-						<label className="titleSelect">SERIES</label>{" "}
-					</div>
-				</Link>
-			</div>
-		</div>
-	);
+      <div className="imgSelect">
+        <Link to="/ListMovies">
+          <div className="labelMovies">
+            <img src={movies} alt="Movies" />
+            <label className="titleSelect"> MOVIES</label>
+          </div>
+        </Link>
+        <Link to="/ListSeries">
+          <div className="labelMovies">
+            <img src={series} alt="Series" />
+            <label className="titleSelect">SERIES</label>{" "}
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
