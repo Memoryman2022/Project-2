806bede027eff20b2a3420bcff1260375b55d237
unknown <unidrelyn@gmail.com>
2024-03-20 10:01:00 +0100

    change movies and series completed

--- frontend/src/pages/ListSeries.jsx
+++ frontend/src/pages/ListSeries.jsx
@@ -5,16 +5,18 @@ import CarouselImg from "../components/CarouselImg";
 import { Link } from "react-router-dom";
 
 export default function ListSeries() {
-  const [seriesList, setSeriesList] = useState([]);
+	const [seriesList, setSeriesList] = useState([]);
 
-  const [genre, setGenre] = useState("");
+	const [genre, setGenre] = useState("");
 
-  /*const apiKey = "3d0dbaba1ed955f27af66d0c59898ec2";
+	const endPoint = "http://localhost:5005/series";
+
+	/*const apiKey = "3d0dbaba1ed955f27af66d0c59898ec2";
 	const tokenApi =
 		"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZDBkYmFiYTFlZDk1NWYyN2FmNjZkMGM1OTg5OGVjMiIsInN1YiI6IjY1ZjFjZDc0NDcwZWFkMDE2MjljNmZjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9uzeZ1bJ9uNWBN-WlFCksLUIOXR9mIeF_IMcr0IczAQ";
 */
-  useEffect(() => {
-    /*const endPoint =
+	useEffect(() => {
+		/*const endPoint =
 			"https://api.themoviedb.org/3/trending/movie/week?language=en-US";
 		axios.defaults.headers.common["Authorization"] = "Bearer " + tokenApi;
 		axios
@@ -29,72 +31,77 @@ export default function ListSeries() {
 				console.error(err);
 			});*/
 
-    const endPoint = "http://localhost:5005/series";
-    axios
-      .get(endPoint)
-      .then((res) => {
-        console.log(res.data);
-        setSeriesList(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+		axios
+			.get(endPoint)
+			.then((res) => {
+				setSeriesList(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 
-  return (
-    <>
-      <section className="total-movies animatedBakground">
-        {genre == "" ? <CarouselImg /> : null}
-        <h2>Select </h2>
-        <form action="">
-          <label>
-            Genre
-            <select
-              name="genre"
-              value={genre}
-              onChange={(e) => setGenre(e.target.value)}
-            >
-              <option value="">-- None --</option>
-              <option value="10759">Action & Adventure</option>
-              <option value="16">Animation</option>
-              <option value="35">Comedy </option>
-              <option value="80">Crime</option>
-              <option value="99">Documentary</option>
-              <option value="18">Drama</option>
-              <option value="10751">Family</option>
-              <option value="10762">Kids</option>
-              <option value="9648">Mystery</option>
-              <option value="10763">News</option>
-              <option value="10764">Reality</option>
-              <option value="10765">Scifi & Fantasy</option>
-              <option value="10766">Soap</option>
-              <option value="10768">War & Politics</option>
-              <option value="37">Western</option>
-            </select>
-          </label>
-        </form>
-        <div className="resultMovies">
-          {genre == ""
-            ? null
-            : seriesList.map((movie, i) => {
-                console.log(movie);
-                if (i < 20) {
-                  return (
-                    <Link to={`/Movies/${movie.id}`} key={movie.id}>
-                      <div key={movie.id} className="movieCard">
-                        <h6>{movie.title}</h6>
-                        <img
-                          className="imgDetail"
-                          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                          width={"100px"}
-                        />
-                      </div>
-                    </Link>
-                  );
-                }
-              })}
-        </div>
-      </section>
-    </>
-  );
+	const genderSelection =
+		genre == ""
+			? ""
+			: seriesList.filter(
+					(ele) => ele.genre_ids.includes(Number(genre)) == true
+			  );
+
+	return (
+		<>
+			<section className="total-movies animatedBakground">
+				{genre == "" ? <CarouselImg endPoint={endPoint} /> : null}
+				<h2>Select </h2>
+				<form action="">
+					<label>
+						Genre
+						<select
+							name="genre"
+							value={genre}
+							onChange={(e) => setGenre(e.target.value)}
+						>
+							<option value="">-- None --</option>
+							<option value="10759">Action & Adventure</option>
+							<option value="16">Animation</option>
+							<option value="35">Comedy </option>
+							<option value="80">Crime</option>
+							<option value="99">Documentary</option>
+							<option value="18">Drama</option>
+							<option value="10751">Family</option>
+							<option value="10762">Kids</option>
+							<option value="9648">Mystery</option>
+							<option value="10763">News</option>
+							<option value="10764">Reality</option>
+							<option value="10765">Scifi & Fantasy</option>
+							<option value="10766">Soap</option>
+							<option value="10768">War & Politics</option>
+							<option value="37">Western</option>
+						</select>
+					</label>
+				</form>
+				<div className="resultMovies">
+					{genderSelection == ""
+						? null
+						: genderSelection.map((movie, i) => {
+								console.log(movie);
+								if (i < 20) {
+									return (
+										<Link to={`/SerieSelect/${movie.id}`} key={movie.id}>
+											<div key={movie.id} className="movieCard">
+												<h6>{movie.name}</h6>
+												<img
+													className="imgDetail"
+													src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+													width={"100px"}
+												/>
+											</div>
+										</Link>
+									);
+								}
+						  })}
+				</div>
+			</section>
+		</>
+	);
 }
